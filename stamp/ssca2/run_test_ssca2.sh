4dfc921f03af396ac7d696f49a1a2bbd437ad22d
Fix vm state in balancer <disanzo@dis.uniroma1.it>
2016-01-15 16:52:24 +0100

    a

--- stamp/ssca2/run_test_ssca2.sh
+++ stamp/ssca2/run_test_ssca2.sh
@@ -15,7 +15,7 @@ while [ $nthread -le $maxThread ]
 	while [ $k -lt $runPerThread ]
 		do
 		echo nice -20 ./ssca2 -s20 -i1 -u1 -l3 -p3 -t$nthread 			
-		nice -20 ./ssca2 ssca2 -s20 -i1 -u1 -l3 -p3 -t$nthread >> results_analysis.txt
+		nice -20 ./ssca2 -s20 -i1 -u1 -l3 -p3 -t$nthread >> results_analysis.txt
 		k=$[$k+1]
 	done
 	nthread=$[$nthread+1]
@@ -30,7 +30,7 @@ while [ $nthread -le $maxThread ]
         while [ $k -lt $runPerThread ]
                 do
                 echo nice -20 ./ssca2 -s18 -i1 -u1 -l3 -p3 -t$nthread                       
-                nice -20 ./ssca2 -v32 -s18 -i1 -u1 -l3 -p3 -t$nthread >> results_analysis.txt
+                nice -20 ./ssca2 -s18 -i1 -u1 -l3 -p3 -t$nthread >> results_analysis.txt
                 k=$[$k+1]
         done
         nthread=$[$nthread+1]
@@ -45,7 +45,7 @@ while [ $nthread -le $maxThread ]
         while [ $k -lt $runPerThread ]
                 do
                 echo nice -20 ./ssca2 -s19 -i1 -u1 -l9 -p9 -t$nthread                       
-                nice -20 ./ssca2 -v32 -s19 -i1 -u1 -l9 -p9 -t$nthread >> results_analysis.txt
+                nice -20 ./ssca2 -s19 -i1 -u1 -l9 -p9 -t$nthread >> results_analysis.txt
                 k=$[$k+1]
         done
         nthread=$[$nthread+1]
@@ -60,7 +60,7 @@ while [ $nthread -le $maxThread ]
         while [ $k -lt $runPerThread ]
                 do
                 echo nice -20 ./ssca2 -s17 -i1 -u1 -l9 -p9 -t$nthread                       
-                nice -20 ./ssca2 -v32 -s17 -i1 -u1 -l9 -p9 -t$nthread >> results_analysis.txt
+                nice -20 ./ssca2 -s17 -i1 -u1 -l9 -p9 -t$nthread >> results_analysis.txt
                 k=$[$k+1]
         done
         nthread=$[$nthread+1]
