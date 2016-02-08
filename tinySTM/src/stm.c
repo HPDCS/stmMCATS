f80bd071026f5e9086662d436a55c91e5a772d91
io <disanzo@dis.uniroma1.it>
2016-02-08 16:05:07 +0100

    a

--- tinySTM/src/stm.c
+++ tinySTM/src/stm.c
@@ -502,7 +502,7 @@ inline void stm_wait(int id) {
 			//sleeping
 			stm_time_t start, end;
 			start = STM_TIMER_READ();
-			usleep(100);
+			//usleep(100);
 			end = STM_TIMER_READ();
 			printf("\nQueued_transactions-1: %i, Average spin time per waiting transaction %f, product %f, thread slept for ticks=%llu",
 					queued_transactions-1,
