6624ddfda372e671acb28dbec0740a98041a2409
io <disanzo@dis.uniroma1.it>
2016-02-08 18:15:31 +0100

    a

--- stamp/intruder/intruder.c
+++ stamp/intruder/intruder.c
@@ -373,8 +373,8 @@ MAIN(argc, argv)
     //Added for MCATS stats
 	if (getenv("STM_STATS") != NULL) {
 		unsigned long u;
-		#ifdef STM_ENERGY_MONITOR
 		if (stm_get_global_stats("global_nb_commits", &u) != 0)
+		#ifdef STM_ENERGY_MONITOR
 			printf("\tThroughput: %f\n, Joules/tx %f",u/TIMER_DIFF_SECONDS(startTime, stopTime), u/joule );
 		#else
 			printf("\tThroughput: %f\n",u/TIMER_DIFF_SECONDS(startTime, stopTime));
