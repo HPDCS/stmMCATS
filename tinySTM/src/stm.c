a48b8636402d3f4c5b8189f3ee3be12e43844289
io <disanzo@dis.uniroma1.it>
2016-02-09 12:18:34 +0100

    a

--- tinySTM/src/stm.c
+++ tinySTM/src/stm.c
@@ -500,7 +500,7 @@ inline void stm_wait(int id) {
 
 		if (//(tx->i_am_the_collector_thread!=1) &&
 				//((double)(queued_transactions-1)*(double)average_spin_time_per_waiting_transacton>(double)busy_waiting_time_threashold)) {
-				queued_transactions>8) {
+				queued_transactions>busy_waiting_time_threashold) {
 				//sleeping
 			//stm_time_t start, end;
 			//start = STM_TIMER_READ();
