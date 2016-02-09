a2b4456bd9177a051012bb6a96c77865747f4816
io <disanzo@dis.uniroma1.it>
2016-02-09 12:35:55 +0100

    a

--- tinySTM/src/stm.c
+++ tinySTM/src/stm.c
@@ -497,7 +497,7 @@ inline void stm_wait(int id) {
 			tx->queued_transactions++;
 		}
 		//busy waiting or sleeping?
-		printf("\nQueued_transactions: %i", queued_transactions);
+		//printf("\nQueued_transactions: %i", queued_transactions);
 
 		if (//(tx->i_am_the_collector_thread!=1) &&
 				//((double)(queued_transactions-1)*(double)average_spin_time_per_waiting_transacton>(double)busy_waiting_time_threashold)) {
