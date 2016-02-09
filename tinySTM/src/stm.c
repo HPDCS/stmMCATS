0110a763d4fb63e1044e34ae3dcd2937d75a423b
io <disanzo@dis.uniroma1.it>
2016-02-09 16:40:16 +0100

    a

--- tinySTM/src/stm.c
+++ tinySTM/src/stm.c
@@ -639,8 +639,7 @@ inline void stm_tune_scheduler(){
 
 
 	//printf("\nTotal_queued_transactions: %i, Sleepy transactions: %i", total_queued_transactions, total_sleepy_transactions);
-	//printf(" Tx time: %f, Spin time: %f, No tx time %f, tx time ratio %f", (((float)total_tx_time+(float)total_tx_wasted_time))/(float)total_committed_transactions, (float)total_tx_spin_time/(float)total_committed_transactions, (float)total_no_tx_time/(float)total_committed_transactions,
-			tx_time_ratio);
+	//printf(" Tx time: %f, Spin time: %f, No tx time %f, tx time ratio %f", (((float)total_tx_time+(float)total_tx_wasted_time))/(float)total_committed_transactions, (float)total_tx_spin_time/(float)total_committed_transactions, (float)total_no_tx_time/(float)total_committed_transactions,tx_time_ratio);
 	/*
 	float *mu_k=(float*)malloc((max_concurrent_threads+1) * sizeof(float));
 	float lambda = 1.0 / (((float) total_no_tx_time/(float)1000000000)/(float) total_committed_transactions_by_collector_threads);
