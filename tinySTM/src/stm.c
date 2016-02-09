68a50bb1ca9451aa38ec4852331353caedd4ff4e
io <disanzo@dis.uniroma1.it>
2016-02-09 16:56:30 +0100

    a

--- tinySTM/src/stm.c
+++ tinySTM/src/stm.c
@@ -630,15 +630,14 @@ inline void stm_tune_scheduler(){
 		reset_local_stats(thread);
 		thread=thread->next;
 	}
-	printf("\n%llu %llu %llu %llu %i", total_tx_time, total_no_tx_time, total_tx_wasted_time, total_tx_spin_time, total_committed_transactions_by_collector_threads);
 	//for(i=0;i<max_concurrent_threads+1;i++) printf("\nwasted_time_k[%i] %llu", i, wasted_time_k[i]);
 	average_running_transactions=average_running_transactions/(float)total_committed_transactions_by_collector_threads;
 	tx_time_ratio=((float)total_tx_time+(float)total_tx_wasted_time)/
 		((float)total_tx_time+(float)total_tx_wasted_time+(float)total_no_tx_time);
 
-	printf("\nTotal_queued_transactions: %i, Sleepy transactions: %i", total_queued_transactions, total_sleepy_transactions);
-	printf(" Tx time: %f, Spin time: %f, No tx time %f, tx time ratio %f",
-			(((float)total_tx_time+(float)total_tx_wasted_time))/(float)total_committed_transactions_by_collector_threads, (float)total_tx_spin_time/(float)total_committed_transactions_by_collector_threads, (float)total_no_tx_time/(float)total_committed_transactions_by_collector_threads,tx_time_ratio);
+	//printf("\nTotal_queued_transactions: %i, Sleepy transactions: %i", total_queued_transactions, total_sleepy_transactions);
+	//printf(" Tx time: %f, Spin time: %f, No tx time %f, tx time ratio %f",
+			//(((float)total_tx_time+(float)total_tx_wasted_time))/(float)total_committed_transactions_by_collector_threads, (float)total_tx_spin_time/(float)total_committed_transactions_by_collector_threads, (float)total_no_tx_time/(float)total_committed_transactions_by_collector_threads,tx_time_ratio);
 	/*
 	float *mu_k=(float*)malloc((max_concurrent_threads+1) * sizeof(float));
 	float lambda = 1.0 / (((float) total_no_tx_time/(float)1000000000)/(float) total_committed_transactions_by_collector_threads);
