9497a4317a86c1233e5d89da1788d2f0a4736d89
io <disanzo@dis.uniroma1.it>
2016-02-10 14:36:15 +0100

    a

--- tinySTM/src/stm.c
+++ tinySTM/src/stm.c
@@ -455,71 +455,77 @@ _CALLCONV stm_tx_t *stm_pre_init_thread(int id){
 
 inline void stm_wait(int id) {
 
-        TX_GET;
+	TX_GET;
 
-        int active_txs, max_txs;
-        int entered=0;
-        stm_time_t start_spin_time=0;
+	int active_txs, max_txs;
+	int entered = 0;
+	stm_time_t start_spin_time = 0;
+	tx->CAS_executed = 0;
 
-	//check wether executing CAS
-	if (max_concurrent_threads<=max_allowed_running_transactions) {
-		entered=1;
-		tx->CAS_executed=0;
+	//check whether executing CAS
+	if (0) {
+		entered = 1;
 	} else {
-		tx->CAS_executed=1;
-
-        	while(1){
-                	active_txs=running_transactions;
-	                max_txs=max_allowed_running_transactions;
-	                if(active_txs<max_txs){
-	                        if (ATOMIC_CAS_FULL(&running_transactions, active_txs, active_txs+1) != 0){
-	                                if(tx->i_am_the_collector_thread==1){
-	                                        tx->first_start_tx_time=tx->last_start_tx_time=STM_TIMER_READ();
-	                                        tx->total_no_tx_time+=tx->last_start_tx_time - tx->start_no_tx_time;
-	                               }
-	                               entered=1;
-	                               break;
-	                        }
-	                } else break;
-	      	}
+		while (1) {
+			active_txs = running_transactions;
+			max_txs = max_allowed_running_transactions;
+			if (active_txs < max_txs) {
+				if (ATOMIC_CAS_FULL(&running_transactions, active_txs,
+						active_txs + 1)
+						!= 0) {
+					if (tx->i_am_the_collector_thread == 1) {
+						tx->first_start_tx_time = tx->last_start_tx_time =STM_TIMER_READ();
+						tx->total_no_tx_time += tx->last_start_tx_time - tx->start_no_tx_time;
+					}
+					entered = 1;
+					//tx->CAS_executed = 1;
+					break;
+				}
+			} else
+				break;
+		}
 	}
-        
-	if(entered==1 && tx->i_am_the_collector_thread==1){
-        	tx->first_start_tx_time=tx->last_start_tx_time=STM_TIMER_READ();
-                tx->total_no_tx_time+=tx->last_start_tx_time - tx->start_no_tx_time;
-        } else {
 
-                if(tx->i_am_the_collector_thread==1){
-                        //collect statistics
-                        start_spin_time=STM_TIMER_READ();
-                        tx->total_no_tx_time+=start_spin_time - tx->start_no_tx_time;
-                }
-	        //stm_time_t start;
-                //start = STM_TIMER_READ();                       
-                //usleep(10000);
-                //printf("\nSleep time: %llu",  STM_TIMER_READ()-start);
+	if (entered == 1) {
+		if (tx->i_am_the_collector_thread == 1) {
+			tx->first_start_tx_time = tx->last_start_tx_time = STM_TIMER_READ();
+			tx->total_no_tx_time += tx->last_start_tx_time - tx->start_no_tx_time;
+		}
+	} else {
+		if(tx->i_am_the_collector_thread==1){
+			//collect statistics
+			start_spin_time=STM_TIMER_READ();
+			tx->total_no_tx_time+=start_spin_time - tx->start_no_tx_time;
+		}
+		//stm_time_t start;
+		//start = STM_TIMER_READ();
+		//usleep(10000);
+		//printf("\nSleep time: %llu",  STM_TIMER_READ()-start);
 		int i, max_cycles=500000;
 		while(1){
-                        active_txs=running_transactions;
-                        max_txs=max_allowed_running_transactions;
-                        if(active_txs<max_txs)
-                                if (ATOMIC_CAS_FULL(&running_transactions, active_txs, active_txs+1) != 0) break;
-                        tx->i_am_waiting=1;
-			for(i=0;i<max_cycles;i++){
-                        	if(tx->i_am_waiting==0)break;
-                        }
-                        tx->i_am_waiting=0;
-                }
+			active_txs=running_transactions;
+			max_txs=max_allowed_running_transactions;
+			if(active_txs<max_txs)
+				if (ATOMIC_CAS_FULL(&running_transactions, active_txs, active_txs+1)!= 0) {
+					//tx->CAS_executed=1;
+					break;
+				}
+			tx->i_am_waiting=1;
+			for(i=0;i<max_cycles;i++) {
+				if(tx->i_am_waiting==0)break;
+			}
+			tx->i_am_waiting=0;
+		}
 
-                if (tx->i_am_the_collector_thread==1){
-                        tx->first_start_tx_time=tx->last_start_tx_time=STM_TIMER_READ();
-                        tx->total_spin_time+=tx->first_start_tx_time-start_spin_time;
-                }
-        }
+		if (tx->i_am_the_collector_thread==1) {
+			tx->first_start_tx_time=tx->last_start_tx_time=STM_TIMER_READ();
+			tx->total_spin_time+=tx->first_start_tx_time-start_spin_time;
+		}
+	}
 
-        if (tx->i_am_the_collector_thread==1){
-                tx->start_no_tx_time=0;
-        }
+	if (tx->i_am_the_collector_thread==1) {
+		tx->start_no_tx_time=0;
+	}
 }
 
 
