2b9e839d4ee9abaea6c8cfb72390669967379294
io <disanzo@dis.uniroma1.it>
2016-02-12 13:01:11 +0100

    a

--- tinySTM/src/stm.c
+++ tinySTM/src/stm.c
@@ -631,10 +631,10 @@ inline void stm_tune_scheduler(){
 		th_minus_1=get_throughput(lambda,mu_k,m-1);
 	if(th_minus_2 > th && th_minus_2 > th_minus_1 && m>3) {
 		max_allowed_running_transactions-=2;
-		printf("\nSelected th_minus_2");
+		//printf("\nSelected th_minus_2");
 	}else if(th_minus_1>th){
 		max_allowed_running_transactions--;
-		printf("\nSelected th_minus_1");
+		//printf("\nSelected th_minus_1");
 	}else if(m<max_concurrent_threads){
 		float average_restarted_transactions= (float)conflicts_per_active_transactions[m]/(float)committed_per_active_transactions[m];
 		float p_a_k = average_restarted_transactions /(1.0 + average_restarted_transactions);
@@ -651,15 +651,15 @@ inline void stm_tune_scheduler(){
 		th_plus_1 = get_throughput(lambda,mu_k,m + 1);
 		if(th_plus_1 > th) {
 			max_allowed_running_transactions++;
-			printf("\nSelected th_plus_1");
+			//printf("\nSelected th_plus_1");
 		} else {
-			printf("\nSelected th");
+			//printf("\nSelected th");
 		}
 	}//
 
 	tx->start_no_tx_time=STM_TIMER_READ();
-	printf("\nPredicted: %f|%f|%f|%f, measured: %f, max txs: %i", th_minus_2, th_minus_1, th, th_plus_1, (float)total_committed_transactions/((float)(now-last_tuning_time)/(float)1000000000), max_allowed_running_transactions);
-	printf("\tTotal commits: %i (as a collector: %i)",total_committed_transactions, total_committed_transactions_by_collector_threads);
+	//printf("\nPredicted: %f|%f|%f|%f, measured: %f, max txs: %i", th_minus_2, th_minus_1, th, th_plus_1, (float)total_committed_transactions/((float)(now-last_tuning_time)/(float)1000000000), max_allowed_running_transactions);
+	//printf("\tTotal commits: %i (as a collector: %i)",total_committed_transactions, total_committed_transactions_by_collector_threads);
 	//printf("\nlambda: %f mu: %f", lambda, 1.0 / ((((float)total_tx_wasted_time/(float)1000000000)/(float)total_committed_transactions_by_collector_threads)+(((float)total_tx_time/(float)1000000000) / (float) total_committed_transactions_by_collector_threads)));
 	//printf("\naverage_running_transactions: %f", average_running_transactions, 1.0);
 	//fflush(stdout);
